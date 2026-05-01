70a3f8ce2ce64b627d957d7e27041cdf6e8885a8
vishalicious213 <vishalicious213@gmail.com>
2026-05-01 12:38:02 -0700

    server: brought in path & used it in app to find the folder with pics

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
-const app = express()
+const path = require('path')
 const cors = require('cors')
+
+const app = express()
 const PORT = 8000
 
 app.use(cors())
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
